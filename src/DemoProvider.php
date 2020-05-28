8bdc5346f1336b21d27424e36b924fd5bf80d937
Mike Hemberger <mike@thestizmedia.com>
2020-05-28 11:31:15 -0400

    fix illegal offset if no demos
    Related to https://github.com/maithemewp/mai-setup-wizard/issues/2 but doesn’t fix

--- src/DemoProvider.php
+++ src/DemoProvider.php
@@ -57,6 +57,9 @@ class DemoProvider extends AbstractServiceProvider {
 	}
 
 	public function get_chosen_demo() {
+		if ( ! $this->demos ) {
+			return '';
+		}
 		$options = \get_option( $this->plugin->slug, [] );
 		$first   = \reset( $this->demos );
 
